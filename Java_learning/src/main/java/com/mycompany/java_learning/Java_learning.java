7bb367490468ae8b02165bed76b52e021507d82b
Andriyanov322 <BombitGame@gmail.com>
2023-01-12 19:13:41 +0500

    Hello world

---
+++ Java_learning/src/main/java/com/mycompany/java_learning/Java_learning.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java_learning;
+
+/**
+ *
+ * @author maksim
+ */
+public class Java_learning {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Worl!");
+    }
+}
