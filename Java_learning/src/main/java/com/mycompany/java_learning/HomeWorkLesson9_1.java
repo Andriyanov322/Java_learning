f519a15dc03866c813bbbbfeb51f9a0360e29e83
Andriyanov322 <BombitGame@gmail.com>
2023-01-28 21:28:26 +0500

    Do some learning tasks

---
+++ Java_learning/src/main/java/com/mycompany/java_learning/HomeWorkLesson9_1.java
@@ -0,0 +1,20 @@
+package com.mycompany.java_learning;
+
+public class HomeWorkLesson9_1 {
+    public static void main(String[] args) {
+        HomeWorkLesson9_1 object1 = new HomeWorkLesson9_1();
+        HomeWorkLesson9_1 object2 = new HomeWorkLesson9_1();
+        HomeWorkLesson9_1 object3 = new HomeWorkLesson9_1();
+        HomeWorkLesson9_1 object4 = new HomeWorkLesson9_1();
+        HomeWorkLesson9_1 object5 = new HomeWorkLesson9_1();
+        HomeWorkLesson9_1 object6 = new HomeWorkLesson9_1();
+        HomeWorkLesson9_1 object7 = new HomeWorkLesson9_1();
+        HomeWorkLesson9_1 object8 = new HomeWorkLesson9_1();  
+        object1 = null;
+        object2 = null;
+        object3 = null;
+        object4 = null;
+        object5 = null;
+        object6 = null;       
+    }
+}
